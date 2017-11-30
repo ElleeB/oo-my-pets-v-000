cf82996e792fb4736f5e0e6761fa316a74dae683
Laura Trager <laura.trager@gmail.com>
2017-11-30 19:43:13 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
 class Owner
-  # code goes here
+
+  attr_accessor :name, :pet
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(name)
+    @pets = {}
+    @@all << self
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def species
+    @species = species
+  end
+
+
+
+
 end
